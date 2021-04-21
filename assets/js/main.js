27f3cc88a14c68a4f9d6e80809fb258f69b427c6
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-04-21 15:03:08 +0200

    aggiunto JS (writeln, alert, getElementById

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/* write */
+document.writeln("Questo è stato scritto con JavaScript");
+
+/* alert */
+alert("Attenzione! Ti trovi su Spotify, ma non quello vero.");
+
+/* get Element */
+document.getElementById("title_music").innerHTML = "Titolo Musica (JS)";
